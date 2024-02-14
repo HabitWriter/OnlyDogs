3d68c964e38d3359dff1c1fa1e4c7bd128daabdb
Robert Smith <r.reed.smith@gmail.com>
2024-02-13 20:50:56 -0700

    Going to start on DB stuff

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,22 @@
 export default function Home() {
-    
-
     return (
-        <div>Only Dogs</div>
+        <div>
+            <div>Only Dogs</div>
+            <div className="card w-96 bg-base-100 shadow-xl mx-40">
+                <figure>
+                    <img
+                        src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                        alt="Shoes"
+                    />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
