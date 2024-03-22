64a84626ecc1015bfa506c4dd60ae6aefc79e9d9
Robert Smith <r.reed.smith@gmail.com>
2024-03-21 18:58:25 -0600

    Got posts working properly in DB and in state.

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -25,6 +25,49 @@ postRouter.get("/all", async (req, res) => {
     res.json(allPosts);
 });
 
+postRouter.post("/like", async (req,res) => {
+  const {postId} = req.body;
+  const currentPost = await Post.findByPk(postId)
+
+  // console.log(req.body);
+  // console.log(currentPost);
+  currentPost.likes++
+
+  currentPost.save();
+
+  res.json(currentPost);
+
+})
+
+postRouter.post("/new", async (req,res) => {
+    const {userId, body, image} = req.body;
+    const currentUser = await User.findByPk(userId)
+    const newPost = await Post.create({ body: body, image: image})
+    await currentUser.addPost(newPost)
+
+    // Reload the post to include the associated user
+    const postWithUser = await Post.findByPk(newPost.postId, {
+        include: [
+            {
+                model: Comment,
+                as: "comments", // Optional: this will alias the relation as 'comments' in the returned data
+                include: [
+                    {
+                        model: User,
+                        as: "user", // Optional: this will alias the relation as 'user' in the returned data
+                    },
+                ],
+            },
+            {
+                model: User,
+                as: "user", // Optional: this will alias the relation as 'user' in the returned data
+            },
+        ]
+    })
+    // console.log(postWithUser);
+    res.json(postWithUser)
+})
+
 // topicRouter.post('/new', async (req, res) => {
 //     const {title} = req.body;
 
